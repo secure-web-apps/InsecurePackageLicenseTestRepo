c446d88783ffdd55ea6413ac60ea87985f2835d3
damienbod <damien_bod@hotmail.com>
2026-04-01 09:24:16 +0200

    Update projects

---
+++ tests/UserEndpointsTests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using System.Net.Http.Json;
+
+namespace BffMicrosoftEntraID.Server.IntegrationTests;
+
+public class UserEndpointsTests : IClassFixture<WebApplicationFactory<Program>>
+{
+    private readonly WebApplicationFactory<Program> _factory;
+
+    public UserEndpointsTests(WebApplicationFactory<Program> factory)
+    {
+        _factory = factory;
+    }
+
+    [Fact]
+    public async Task Get_AsUnauthenticatedUser_ReturnsAnonymousUserInfo()
+    {
+        // Arrange
+        var client = _factory.CreateClient();
+
+        // Act
+        var response = await client.GetAsync("/api/user");
+
+        // Assert
+        response.EnsureSuccessStatusCode();
+        var userInfo = await response.Content.ReadFromJsonAsync<object>();
+        Assert.NotNull(userInfo);
+    }
+}
